46e0ab4a096cf9ac213e50506777abf6a5b81761
Jaime Niswonger <jniswonger@keyholesoftware.com>
2022-04-22 20:52:46 +0000

    azure deploy working

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,6 @@ const defaultPort = 4000;
 interface Environment {
     apollo: {
         introspection: boolean,
-        isLambda: boolean
     },
     port: number | string;
 }
@@ -11,7 +10,6 @@ interface Environment {
 export const environment: Environment = {
     apollo: {
         introspection: process.env.APOLLO_INTROSPECTION === 'true',
-        isLambda: process.env.APOLLO_LAMBDA === 'true' || true
     },
     port: process.env.PORT || defaultPort
 };
