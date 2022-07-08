af8afc0e4f38a4134d7a9bc7b3fe88c7ce8eb5c3
Randy Fox <rfox@keyholesoftware.com>
2022-07-08 13:45:22 -0500

    Changed to a const for URL prefix.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,28 +1,33 @@
 import { nowPlaying, movieById } from "./resolver/movies";
 
+const posterURLPrefix = 'https://www.themoviedb.org/t/p/';
+
 export default {
+  
+
   Query: {
     nowPlaying: nowPlaying,
     movie: movieById
   },
   Movie: {
+    
     posterPath: (parent: any, args: any, context: any): String => {
-      return `https://www.themoviedb.org/t/p/w500${parent.poster_path}`;
+      return posterURLPrefix + `w500${parent.poster_path}`;
     },
     posterPathW92: (parent: any, args: any, context: any): String => {
-      return `https://www.themoviedb.org/t/p/w92${parent.poster_path}`;
+      return posterURLPrefix + `w92${parent.poster_path}`;
     },
     posterPathW154: (parent: any, args: any, context: any): String => {
-      return `https://www.themoviedb.org/t/p/w154${parent.poster_path}`;
+      return posterURLPrefix + `w154${parent.poster_path}`;
     },
     posterPathW185: (parent: any, args: any, context: any): String => {
-      return `https://www.themoviedb.org/t/p/w185${parent.poster_path}`;
+      return posterURLPrefix + `w185${parent.poster_path}`;
     },
     posterPathW342: (parent: any, args: any, context: any): String => {
-      return `https://www.themoviedb.org/t/p/w342${parent.poster_path}`;
+      return posterURLPrefix + `w342${parent.poster_path}`;
     },
     posterPathW780: (parent: any, args: any, context: any): String => {
-      return `https://www.themoviedb.org/t/p/w780${parent.poster_path}`;
+      return posterURLPrefix + `w780${parent.poster_path}`;
     },
   },
 };
