9e22fcf7ae8081cbc4e6f6da83df6fd41a20fe46
Randy Fox <rfox@keyholesoftware.com>
2022-07-11 15:52:56 +0000

    Changed const name from posterURLPrefix to imageURLPrefix.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,6 @@
 import { nowPlaying, movieById } from "./resolver/movies";
 
-const posterURLPrefix = 'https://image.tmdb.org/t/p/';
+const imageURLPrefix = 'https://image.tmdb.org/t/p/';
 
 export default {
   
@@ -11,31 +11,31 @@ export default {
   },
   Movie: {
     posterPath: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w500${parent.poster_path}`;
+      return `${imageURLPrefix}w500${parent.poster_path}`;
     },
     posterPathW92: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w92${parent.poster_path}`;
+      return `${imageURLPrefix}w92${parent.poster_path}`;
     },
     posterPathW154: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w154${parent.poster_path}`;
+      return `${imageURLPrefix}w154${parent.poster_path}`;
     },
     posterPathW185: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w185${parent.poster_path}`;
+      return `${imageURLPrefix}w185${parent.poster_path}`;
     },
     posterPathW342: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w342${parent.poster_path}`;
+      return `${imageURLPrefix}w342${parent.poster_path}`;
     },
     posterPathW780: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w780${parent.poster_path}`;
+      return `${imageURLPrefix}w780${parent.poster_path}`;
     },
     backdropPathW300: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w300${parent.backdrop_path}`;
+      return `${imageURLPrefix}w300${parent.backdrop_path}`;
     },
     backdropPathW780: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w780${parent.backdrop_path}`;
+      return `${imageURLPrefix}w780${parent.backdrop_path}`;
     },
     backdropPathW1280: (parent: any, args: any, context: any): String => {
-      return `${posterURLPrefix}w1280${parent.backdrop_path}`;
+      return `${imageURLPrefix}w1280${parent.backdrop_path}`;
     },
     
   },
