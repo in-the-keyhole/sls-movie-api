da2cd42f6bebf11526c3c6e1699efae8ae1546f8
jdehan2 <jdehan2@gmail.com>
2022-09-20 17:55:17 +0000

    Fix to posters on now-playing

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -71,12 +71,60 @@ export default {
   },
   Cast: {
     profilePath: (parent: any, args: any, context: any): String => {
+      if (parent.profile_path == null) {
+        return '';
+      }
       return `${imageURLPrefix}w92${parent.profile_path}`;
     },
   },
   MovieSummary: {
     details: (parent: any, args: any, context: any): Promise<Movie> => {
       return getMovie(parent.id);
+    },
+    posterPath: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w500${parent.poster_path}`;
+    },
+    posterPathW92: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w92${parent.poster_path}`;
+    },
+    posterPathW154: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w154${parent.poster_path}`;
+    },
+    posterPathW185: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w185${parent.poster_path}`;
+    },
+    posterPathW342: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w342${parent.poster_path}`;
+    },
+    posterPathW780: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w780${parent.poster_path}`;
+    },
+    backdropPathW300: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w300${parent.backdrop_path}`;
+    },
+    backdropPathW780: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w780${parent.backdrop_path}`;
+    },
+    backdropPathW1280: (parent: any, args: any, context: any): String => {
+      return `${imageURLPrefix}w1280${parent.backdrop_path}`;
+    },
+    voteAverage: (parent: any, args: any, context: any): String => {
+      return `${parent.vote_average}`;
+    },
+    trailer: async (
+      parent: any,
+      args: any,
+      context: any
+    ): Promise<Trailer | undefined> => {
+      return await trailer(parent.id);
+    },
+
+    credits: async (
+      parent: any,
+      args: any,
+      context: any
+    ): Promise<Credits | undefined> => {
+      return await credits(parent.id);
     },
   },
 };
